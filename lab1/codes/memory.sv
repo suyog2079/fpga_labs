db0746508dbc1bcbcec2898679bf491bbb90a02b
suyog2079 <079bct091.suyog@pcampus.edu.np>
2026-06-26 00:01:48 +0545

    changed memory from having separate read and write lines to single data line

--- lab1/codes/memory.sv
+++ lab1/codes/memory.sv
@@ -1,21 +1,20 @@
 module memory #(
-    parameter int AD_LINES = 16,
+    parameter int AD_LINES   = 16,
     parameter int DATA_LINES = 8
 ) (
     input logic rd_wr_bar,
     input logic [AD_LINES - 1 : 0] addr,
-    input logic [DATA_LINES - 1 : 0] wd,
-    output logic [DATA_LINES - 1: 0] rd,
+    inout wire [DATA_LINES - 1 : 0] data,
+    //output logic [DATA_LINES - 1: 0] rd,
     input logic clk,
     input logic cs
 );
 
-  logic [DATA_LINES - 1: 0] mem[2**AD_LINES -1];
+  logic [DATA_LINES - 1:0] mem[0:(1<<AD_LINES) -1];
+  assign data = (cs && rd_wr_bar) ? mem[addr] : 'z;
   always_ff @(posedge clk) begin
-    if (cs && rd_wr_bar) begin
-      rd <= mem[addr];
-    end else if (cs && !rd_wr_bar) begin
-      mem[addr] <= wd;
+    if (cs && !rd_wr_bar) begin
+      mem[addr] <= data;
     end
   end
 endmodule

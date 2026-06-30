b414c5c5b9617508231d36d7d9075ab6a8ebb8fc
suyog2079 <079bct091.suyog@pcampus.edu.np>
2026-07-01 00:17:28 +0545

    added control unit

--- lab2/codes/control_unit.sv
+++ lab2/codes/control_unit.sv
@@ -12,55 +12,106 @@ module control_unit #(
 
     // register bank control lines and signals
     output logic [2:0] reg_sel[1:0],
-    output alu_alu_db_bar,
-    output alu_rd_wr_bar,
-    output mv,
-    output logic reg_cs
+    output reg_alu_db_bar,
+    output reg_rd_wr_bar,
+    output swp,
+    output logic reg_cs,
+
+    // alu control bits
+    output logic [3:0] alu_sel
+
 );
 
-  reg pc;
-  reg ir;
+  reg [ AD_LINES - 1 : 0] pc;
+  reg [  AD_LINES -1 : 0] mem_address;
+  reg [DATA_LINES -1 : 0] ir;
 
   typedef enum {
     FETCH,
     DECODE,
     EXECUTE,
+    FETCH_ADDR,
     STORE
   } state_t;
-  state_t state = FETCH;
+  state_t state;
 
-  logic   recv_data = data;
+  typedef enum {
+    SWAP,
+    // LOAD,
+    // STORE,
+    ARITH
+  } inst_t;
+  inst_t instruction_type;
+
+  typedef enum {
+    MEMORY_2_REG,
+    REG_2_MEMORY,
+    ALU_2_REG
+  } store_t;
+  store_t store_type;
+
+  typedef enum {
+    FIRST,
+    SECOND
+  } fetch_number_t;
+  fetch_number_t fetch_number;
+
+  logic [DATA_LINES -1:0] recv_data;
+  assign recv_data = data_bus;
+
+  initial begin
+    pc <= 4'h0;
+    state <= FETCH;
+  end
 
   always_ff @(posedge clk) begin
+    mem_cs         <= 0;
+    mem_rd_wr_bar  <= 1;
+
+    reg_cs         <= 0;
+    swp            <= 0;
+    reg_rd_wr_bar  <= 1;
+    reg_alu_db_bar <= 0;
     case (state)
       FETCH: begin
-        mem_addr <= pc;
-        rd_wr_bar <= 1'b1;
+        mem_cs <= 1;
+        addr_bus <= pc;
+        mem_rd_wr_bar <= 1'b1;
         pc <= pc + 1;
         state <= DECODE;
       end
 
       DECODE: begin
-        ir <= recv_data;
-
+        ir = recv_data;
         case (ir[7:7])
-
-          // either move tyoe or load type instruction
+          // either move type or load type instruction
           1'b0: begin
             case (ir[6:6])
-              // move type instruction
+              // swap type instruction
               // 00, 3 source register select and 3 destination register
               // select
               1'b0: begin
-
+                instruction_type <= SWAP;
+                state <= EXECUTE;
               end
               // load/store type instruction
               // bit division not done yet
               1'b1: begin
-
+                fetch_number <= FIRST;
+                case (ir[5:5])
+                  1'b0: begin
+                    store_type <= MEMORY_2_REG;
+                  end
+                  1'b1: begin
+                    store_type <= REG_2_MEMORY;
+                  end
+                  default: begin
+                    state <= FETCH;
+                  end
+                endcase
+                state <= FETCH_ADDR;
               end
               default: begin
-
               end
             endcase
           end
@@ -68,28 +119,102 @@ module control_unit #(
           // arithematic / logical type instruction
           //1, 3 source register select and 4 operation select bits
           1'b1: begin
-
+            instruction_type <= ARITH;
+            state <= EXECUTE;
           end
-
           default: begin
+            state <= FETCH;
+          end
+        endcase
+      end
 
+      FETCH_ADDR: begin
+        case (fetch_number)
+          FIRST: begin
+            mem_cs <= 1;
+            addr_bus <= pc;
+            mem_rd_wr_bar <= 1'b1;
+            pc <= pc + 1;
+            fetch_number <= SECOND;
+          end
+          SECOND: begin
+            mem_cs <= 1;
+            mem_address[AD_LINES-DATA_LINES-1 : 0] <= recv_data;  //lower oeder address
+            addr_bus <= pc;
+            mem_rd_wr_bar <= 1'b1;
+            pc <= pc + 1;
+            state <= STORE;
+          end
+          default: begin
+            state <= FETCH;
           end
         endcase
       end
 
       EXECUTE: begin
-
+        case (instruction_type)
+          SWAP: begin
+            reg_sel[0] <= ir[5:3];
+            reg_sel[1] <= ir[2:0];
+            reg_cs <= 1;
+            swp <= 1;
+            state <= FETCH;
+          end
+          ARITH: begin
+            alu_sel <= ir[6:3];
+            reg_sel[0] <= 3'b000;
+            reg_sel[1] <= ir[2:0];
+            reg_cs <= 1;
+            reg_alu_db_bar <= 1;
+            reg_rd_wr_bar <= 1;
+            state <= STORE;
+            store_type <= ALU_2_REG;
+          end
+          default: begin
+            state <= FETCH;
+          end
+        endcase
       end
 
       STORE: begin
+        if (store_type != ALU_2_REG) begin
+          mem_address[AD_LINES-1 : AD_LINES-DATA_LINES] <= recv_data;  //higher order address
+        end
+        case (store_type)
+          ALU_2_REG: begin
+            reg_cs <= 1;
+            reg_rd_wr_bar <= 0;
+            reg_alu_db_bar <= 1;
+            reg_sel[0] <= 3'b000;
+          end
+          MEMORY_2_REG: begin
+            reg_cs <= 1;
+            reg_rd_wr_bar <= 0;
+            reg_alu_db_bar <= 0;
+            reg_sel[0] <= 3'b000;
 
+            addr_bus <= mem_address;
+            mem_cs <= 1;
+            mem_rd_wr_bar <= 1;
+          end
+          REG_2_MEMORY: begin
+            reg_cs <= 1;
+            reg_rd_wr_bar <= 1;
+            reg_alu_db_bar <= 0;
+            reg_sel[0] <= 3'b000;
+
+            addr_bus <= mem_address;
+            mem_cs <= 1;
+            mem_rd_wr_bar <= 0;
+          end
+          default: begin
+            state <= FETCH;
+          end
+        endcase
+        state <= FETCH;
       end
-
       default: state <= FETCH;
-
     endcase
-
   end
-
 endmodule
 
--- lab2/codes/register_bank.sv
+++ lab2/codes/register_bank.sv
@@ -6,13 +6,12 @@ module register_bank #(
     input logic clk,
     input alu_db_bar,
     input rd_wr_bar,
-    input mv,
+    input swp,
     input logic [2:0] reg_sel[1:0],
     input logic cs,
     input [DATA_LINES - 1:0] alu_input,
     output logic [DATA_LINES - 1 : 0] alu_out[1:0]
 );
-  reg [7:0] temp;
   reg [DATA_LINES - 1:0] register[0:NO_REGISTERS - 1];
   // connect data_bus to selected register to out selected register value to
   // the data bus
@@ -21,8 +20,8 @@ module register_bank #(
   assign alu_out[1] = register[reg_sel[1]];
   always_ff @(posedge clk) begin : blockName
     if (cs) begin
-      if (mv) begin
-        temp <= register[reg_sel[0]];
+      if (swp) begin
+        temp = register[reg_sel[0]];
         register[reg_sel[0]] <= register[reg_sel[1]];
         register[reg_sel[1]] <= temp;
       end else if (!rd_wr_bar) begin
